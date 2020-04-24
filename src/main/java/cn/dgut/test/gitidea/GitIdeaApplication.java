b9c7d3a02f76a3cea7c8660e2712f1d70407f449
tom_glb <goodMorning_glb@atguigu.com>
2020-04-24 21:31:23 +0800

    第2次提交

--- src/main/java/cn/dgut/test/gitidea/GitIdeaApplication.java
+++ src/main/java/cn/dgut/test/gitidea/GitIdeaApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitIdeaApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(GitIdeaApplication.class, args);
+        System.out.println("test");
     }
 
 }
