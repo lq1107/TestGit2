ac2912a8488ba68b5c08386cf92b8d6dad38cb66
tom_glb <goodMorning_glb@atguigu.com>
2020-04-24 21:34:49 +0800

    第3次提交

---
+++ src/test/java/cn/dgut/test/gitidea/GitIdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.dgut.test.gitidea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIdeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
